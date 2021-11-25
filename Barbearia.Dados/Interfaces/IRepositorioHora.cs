9f45d968f8cd5d0184886d9207a6646a2bfe5b40
Nelson Aparecido Fornazeiro <nfornazeiro@sep.sp.gov.br>
2021-11-25 16:58:02 -0300

    implemento da lista de horas

---
+++ Barbearia.Dados/Interfaces/IRepositorioHora.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barbearia.Dados.Interfaces
+{
+    public interface IRepositorioHora
+    {
+        List<Entidades.Horas> ListarHoras(DateTime data);
+    }
+}
---
+++ Barbearia.Negocios/Hora.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Barbearia.Negocios
+{
+    public class Hora
+    {
+        public List<Entidades.Horas> ListarHoras(DateTime data)
+        {
+            Dados.Repositorios.RepositorioHora nHora = new Dados.Repositorios.RepositorioHora();
+            return nHora.ListarHoras(data);
+        }
+    }
+}

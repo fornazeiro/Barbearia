cd8df4e8e7709ca41476d1ae10ffbb1cee588402
Nelson Aparecido Fornazeiro <nfornazeiro@sep.sp.gov.br>
2021-11-25 15:28:38 -0300

    implemento dos cruds da aplicação(clientes)

--- Barbearia.Entidades/Cliente.cs
+++ Barbearia.Entidades/Cliente.cs
@@ -1,13 +1,19 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Barbearia.Entidades
 {
     public class Cliente
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "O campo Nome é obrigatório.")]
         public string Nome { get; set; }
+        [Required(ErrorMessage = "O campo Data Nascimento é obrigatório.")]
         public DateTime DataNascimento { get; set; }
+        [Required(ErrorMessage = "O campo E-mail é obrigatório.")]
+        [DataType("Email")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "O campo Telefone é obrigatório.")]
         public string Telefone { get; set; }
         public bool Situacao { get; set; }
     }
--- Barbearia.Entidades/Usuario.cs
+++ Barbearia.Entidades/Usuario.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Barbearia.Entidades
 {
     public class Usuario
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "O campo Nome é obrigatório.")]
         public string Nome { get; set; }
+        [Required(ErrorMessage = "O campo E-mail é obrigatório.")]
+        [DataType("Email")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "O campo Senha é obrigatório.")]
         public string Senha { get; set; }
         public bool Situacao { get; set; }
     }
